08b58ae5affdf3fac39f0f3254a9aaca8d71a777
solovyova-1996 <78434580+solovyova-1996@users.noreply.github.com>
2022-02-05 22:27:35 +0600

    dz 8

--- lesson-3/server.py
+++ lesson-3/server.py
@@ -9,7 +9,7 @@ from select import select
 from general.utils import get_messages, send_mesages
 from general.variables import ACTION, GREETINGS, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, PORT_ARGV, DEFAULT_PORT, IP_ARGV, MAX_CONNECTIONS, MESSAGE, \
-    MESSAGE_TEXT, SENDER
+    MESSAGE_TEXT, SENDER, RESPONSE_200, RESPONSE_400, DESTINATION, EXIT
 
 from config import server_log_config
 from decorators_log import log_func
@@ -17,23 +17,67 @@ from decorators_log import log_func
 log = getLogger('server')
 
 
-@log_func
-def handler_client_messages(messages, messages_list, client):
-    # проверка есть ли в сообщении(словаре) ключи: 'action', 'time', 'user' и что находиться в этих полях
-    if ACTION in messages and messages[
-        ACTION] == GREETINGS and TIME in messages and USER in messages and \
-            messages[USER][ACCOUNT_NAME] == 'Guest':
+def handler_messages(message, names_clients, listen_socks):
+    '''
+    Функция проверяет есть ли на сервере клиент, которому отправляется сообщение
+    :param message: словарь сообщения
+    :param names_clients: словарь зарегистрированных пользователей и их сокетов
+    :param listen_socks: список клиентов ожидающих сообщения
+    :return:
+    '''
+    if message[DESTINATION] in names_clients and names_clients[
+        message[DESTINATION]] in listen_socks:
+        send_mesages(names_clients[message[DESTINATION]], message)
         log.info(
-            'Клиент- %(account_name)s подключился к серверу(отправил корректный запрос)',
-            messages[USER])
-        send_mesages(client, {RESPONSE: 200})  # return {RESPONSE: 200}
-    elif ACTION in messages and messages[
-        ACTION] == MESSAGE and TIME in messages and MESSAGE_TEXT in messages:
-        messages_list.append((messages[ACCOUNT_NAME], messages[MESSAGE_TEXT]))
-    else:
-        send_mesages(client, {RESPONSE: 400, ERROR: 'Bad Request'})
+            f'Отправлено сообщение пользователю - {message[DESTINATION]} от - {message[SENDER]} ')
+    elif message[DESTINATION] in names_clients and names_clients[
+        message[DESTINATION]] not in listen_socks:
         log.error(
-            'Клиент отправил некорректный запрос на подключение')  # return {RESPONSE: 400, ERROR: 'Bad Request'}
+            f'Соединение с {names_clients[message[DESTINATION]]} потеряно')
+        raise ConnectionError
+    else:
+        log.error(
+            f'Отправка сообщения незарегистрированому пользователю невозможна. Пользователь{message[DESTINATION]} - не зарегистрирован на сервере')
+
+
+@log_func
+def handler_client_messages(messages, messages_list, client, clients_list,
+                            names_clients):
+    # проверка если сообщение о присутствии принимаем и отвечаем
+    # print(clients_list)
+    # print(names_clients)
+    if ACTION in messages and messages[
+        ACTION] == GREETINGS and TIME in messages and USER in messages:
+        if messages[USER][ACCOUNT_NAME] not in names_clients.keys():
+            # если клиента нет в словаре добавляем его имя и сокет
+            names_clients[messages[USER][ACCOUNT_NAME]] = client
+            send_mesages(client, RESPONSE_200)
+            log.info(
+                'Клиент- %(account_name)s подключился к серверу(отправил корректный запрос)',
+                messages[USER])
+        else:
+            response = RESPONSE_400
+            response[ERROR] = 'Имя пользователя занято'
+            send_mesages(client, response)
+            clients_list.remove(client)
+            client.close()
+        return
+    elif ACTION in messages and messages[
+        ACTION] == MESSAGE and TIME in messages and MESSAGE_TEXT in messages and SENDER in messages and DESTINATION:
+        messages_list.append(messages)
+        return
+    elif ACTION in messages and messages[
+        ACTION] == EXIT and ACCOUNT_NAME in messages:
+        clients_list.remove(names_clients[messages[ACCOUNT_NAME]])
+        names_clients[messages[ACCOUNT_NAME]].close()
+        del names_clients[messages[ACCOUNT_NAME]]
+        return
+    else:
+        response = RESPONSE_400
+        response[ERROR] = 'Запрос некорректен'
+        send_mesages(client, response)
+        log.error('Клиент отправил некорректный запрос на подключение')
+        return
 
 
 def argv_parser():
@@ -69,8 +113,11 @@ def main():
     sock_1.settimeout(0.5)
     clients_list = list()
     messages_list = list()
+    # словарь с клиентами и их сокетами
+    names_clients = dict()
     # сигнализируем о готовности принимать соединение MAX_CONNECTIONS - количество одновременно обслуживаемых запросов
     sock_1.listen(MAX_CONNECTIONS)
+    print('Сервер запущен')
     while True:
         # accept - принимает запрос на соединение
         try:
@@ -96,22 +143,22 @@ def main():
             for client_with_messages in recv_lst:
                 try:
                     handler_client_messages(get_messages(client_with_messages),
-                                            messages_list, client_with_messages)
+                                            messages_list, client_with_messages,
+                                            clients_list, names_clients)
                 except:
                     # getpeername() - возвращает удаленный Ip-addr
                     log.info(f'Клиент {client_with_messages.getpeername()} '
                              f'отключился от сервера.')
                     clients_list.remove(client_with_messages)
-        if messages_list and send_lst:
-            message = {ACTION: MESSAGE, SENDER: messages_list[0][0],
-                       TIME: time.time(), MESSAGE_TEXT: messages_list[0][1]}
-            del messages_list[0]
-            for expect_client in send_lst:
-                try:
-                    send_mesages(expect_client, message)
-                except:
-                    log.info(f'Клиент {expect_client.getpeername()} отключился')
-                    clients_list.remove(expect_client)
+
+        for i in messages_list:
+            try:
+                handler_messages(i, names_clients, send_lst)
+            except Exception:
+                log.info(f'Клиент {i[DESTINATION]} отключился')
+                clients_list.remove(names_clients[i[DESTINATION]])
+                del names_clients[i[DESTINATION]]
+        messages_list.clear()
 
 
 if __name__ == '__main__':
