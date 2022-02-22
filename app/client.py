c292d4c2b52bea553b40348d02e7ad1872df9511
solovyova-1996 <78434580+solovyova-1996@users.noreply.github.com>
2022-02-22 17:17:12 +0600

    dz3

--- app/client.py
+++ app/client.py
@@ -20,7 +20,7 @@ from metaclasses import ClientVerifier
 
 log = getLogger('client')
 
-
+from server_database import ServerDatabase
 @log_func
 def argv_parser():
     # создаем парсер командной строки
@@ -88,6 +88,8 @@ class Client(metaclass=ClientVerifier):
                 self.create_message(sock, self.clent_name)
             elif command == 'help':
                 self.print_info_help()
+            # elif command == 'users':
+            #     print(ServerDatabase.users_list())
             elif command == 'exit':
                 message_exit = self.create_exit_message(self.clent_name)
                 send_mesages(sock, message_exit)
